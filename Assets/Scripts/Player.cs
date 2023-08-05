3bddd8d5729cb04819b82f62c4f90bf66d2c8e39
Qubus0 <steen.rickmer@gmx.de>
2023-08-05 16:14:57 +0200

    repeat moves when held

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,34 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
     public static event Action OnPlayerMove;
+    [SerializeField] private float playerSpeed = 0.1f;
 
     
     private void Update()
     {
+        // Check for space key press
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            gameObject.transform.Rotate(0, 360f/60f, 0);
-            OnPlayerMove?.Invoke();
+            // Call Move immediately
+            Move();
+            // delay a bit to give the player more control   
+            InvokeRepeating(nameof(Move), playerSpeed * 2, playerSpeed);
         }
+
+        // Check for space key release
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            CancelInvoke(nameof(Move));
+        }
+    }
+
+    private void Move()
+    {
+        gameObject.transform.Rotate(0, 360f / 60f, 0);
+        OnPlayerMove?.Invoke();
     }
 }
