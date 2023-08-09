df26f6bba7129d15c0fddd4a2e125bb0d61c79e3
Qubus0 <steen.rickmer@gmx.de>
2023-08-09 10:40:01 +0200

    change player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,58 +1,67 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
     public static event Action OnPlayerMove;
     public static event Action OnPlayerMoveBackwards;
-    [SerializeField] private float playerSpeed = 0.1f;
+    [SerializeField] private float playerMoveRepeatTime = 0.1f;
+    private float playerSpeedMultiplier = 1f;
 
-    private bool spacePressed = false;
-    private float spaceHeldTime = 0f;
-    [SerializeField] private float holdThreshold = 0.2f;
+    private enum State
+    {
+        None,
+        Pressed,
+        Held,
+    }
+    private State state = State.None;
     
     private int damage = 1;
+    private int direction = 1;
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(Move), 0, playerMoveRepeatTime);
+    }
+
 
     private void Update()
     {
-        // Check if the Space key is pressed
         if (Input.GetKeyDown(KeyCode.Space))
-            spacePressed = true;
-
-        // Check if the Space key is released
-        if (Input.GetKeyUp(KeyCode.Space))
         {
-            if (spaceHeldTime >= holdThreshold)
-                OnHold();
-            else if (spacePressed)
-                OnRelease();
-
-            spacePressed = false;
-            spaceHeldTime = 0f;
+            playerSpeedMultiplier = 1f;
+            CancelInvoke(nameof(Move));
+            InvokeRepeating(nameof(Move), playerMoveRepeatTime, playerMoveRepeatTime);
+            direction *= -1;
+            gameObject.transform.localScale = new Vector3(direction, 1, 1);
         }
+    }
 
-        if (spaceHeldTime >= holdThreshold)
-        {
-            MoveBackwards();
-            spaceHeldTime = 0f;
-            spacePressed = false;
-        }
+    // private void Update()
+    // {
+    //     // Check for space key press
+    //     if (Input.GetKeyDown(KeyCode.Space))
+    //     {
+    //         // Call Move immediately
+    //         state = State.Pressed;
+    //         // delay a bit to give the player more control   
+    //         InvokeRepeating(nameof(Move), holdThreshold, playerMoveRepeatTime);
+    //     }
+    //
+    //     // Check for space key release
+    //     if (Input.GetKeyUp(KeyCode.Space))
+    //     {
+    //         if (state == State.Pressed)
+    //         {
+    //             CancelInvoke(nameof(Move));
+    //             MoveBackwards();
+    //         }
+    //         else if (state == State.Held)
+    //             CancelInvoke(nameof(Move));
+    //     }
+    // }
 
-        // Increment time if the Space key is held down
-        if (spacePressed)
-            spaceHeldTime += Time.deltaTime;
-    }
-    
-    private void OnHold()
-    {
-        MoveBackwards();
-    }
-    
-    private void OnRelease()
-    {
-        Move();
-    }
-    
     private void OnCollisionEnter(Collision collision)
     {
         DealDamage(collision.gameObject);
@@ -63,28 +72,13 @@ public class Player : MonoBehaviour
         if (otherGameObject.TryGetComponent(out Health health))
             health.TakeDamage(damage);
     }
-
-    // private void Update()
-    // {
-    //     // Check for space key press
-    //     if (Input.GetKeyDown(KeyCode.Space))
-    //     {
-    //         // Call Move immediately
-    //         Move();
-    //         // delay a bit to give the player more control   
-    //         InvokeRepeating(nameof(Move), playerSpeed * 2, playerSpeed);
-    //     }
-    //
-    //     // Check for space key release
-    //     if (Input.GetKeyUp(KeyCode.Space))
-    //     {
-    //         CancelInvoke(nameof(Move));
-    //     }
-    // }
-    //
+    
     private void Move()
     {
-        gameObject.transform.Rotate(0, 360f / 60f, 0);
+        playerSpeedMultiplier -= 0.05f;
+        playerSpeedMultiplier = Mathf.Clamp(playerSpeedMultiplier, 0.3f, 1f);
+        
+        gameObject.transform.Rotate(0, direction * (360f / 60f), 0);
         OnPlayerMove?.Invoke();
     }
 
